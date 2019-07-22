899aed2f37ea223c1d50c27b7f8c349dc4ea1c91
yatatsu <yatatsukitagawa@gmail.com>
2019-07-22 16:27:02 +0900

    Validate card cvc input.

---
+++ payjp/src/test/kotlin/jp/pay/android/model/CardCvcInputTest.kt
@@ -0,0 +1,67 @@
+/*
+ *
+ * Copyright (c) 2019 PAY, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package jp.pay.android.model
+
+import org.hamcrest.Matchers.`is`
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+
+@RunWith(ParameterizedRobolectricTestRunner::class)
+class CardCvcInputTest(
+    private val input: String?,
+    private val value: String?
+) {
+    companion object {
+        @JvmStatic
+        @ParameterizedRobolectricTestRunner.Parameters
+        fun data(): List<Array<out Any?>> {
+            return listOf(
+                arrayOf(null as? String, null as? String),
+                arrayOf("", null),
+                arrayOf("abc", null),
+                arrayOf("12", null),
+                arrayOf("12abc", null),
+                arrayOf(" 12 ", null),
+                arrayOf("123", "123"),
+                arrayOf(" 123 ", "123"),
+                arrayOf("1234", "1234"),
+                arrayOf(" 1234 ", "1234")
+            )
+        }
+    }
+
+    private lateinit var cvcInput: CardCvcInput
+
+    @Before
+    fun setUp() {
+        cvcInput = CardCvcInput(input)
+    }
+
+    @Test
+    fun formatInput() {
+        assertThat(cvcInput.value, `is`(value))
+    }
+}
