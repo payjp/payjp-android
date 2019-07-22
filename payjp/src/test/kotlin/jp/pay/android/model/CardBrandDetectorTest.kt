990c97ef7efdad4cde9a72f603f7606ac9e801fd
yatatsu <yatatsukitagawa@gmail.com>
2019-07-22 11:39:15 +0900

    Add card brand detection test case.

--- payjp/src/test/kotlin/jp/pay/android/model/CardBrandDetectorTest.kt
+++ payjp/src/test/kotlin/jp/pay/android/model/CardBrandDetectorTest.kt
@@ -39,13 +39,70 @@ class CardBrandDetectorTest(
         @ParameterizedRobolectricTestRunner.Parameters
         fun data(): List<Array<out Any?>> {
             return listOf(
-                arrayOf("4242424242424242", CardBrand.VISA),
+                // UNKNOWN
                 arrayOf("0", CardBrand.UNKNOWN),
                 arrayOf("9999", CardBrand.UNKNOWN),
                 arrayOf("77", CardBrand.UNKNOWN),
                 arrayOf("1234", CardBrand.UNKNOWN),
                 arrayOf("111122223333444455556666", CardBrand.UNKNOWN),
-                arrayOf("99999999999999999999999999999999999999999999999999", CardBrand.UNKNOWN)
+                arrayOf("99999999999999999999999999999999999999999999999999", CardBrand.UNKNOWN),
+                // VISA
+                arrayOf("4", CardBrand.VISA),
+                arrayOf("42", CardBrand.VISA),
+                arrayOf("4211", CardBrand.VISA),
+                arrayOf("4242111122223333", CardBrand.VISA),
+                arrayOf("42421111222233334444", CardBrand.VISA),
+                // MASTER_CARD
+                arrayOf("51", CardBrand.MASTER_CARD),
+                arrayOf("52", CardBrand.MASTER_CARD),
+                arrayOf("53", CardBrand.MASTER_CARD),
+                arrayOf("54", CardBrand.MASTER_CARD),
+                arrayOf("55", CardBrand.MASTER_CARD),
+                arrayOf("5151", CardBrand.MASTER_CARD),
+                arrayOf("2221", CardBrand.MASTER_CARD),
+                arrayOf("2230", CardBrand.MASTER_CARD),
+                arrayOf("2300", CardBrand.MASTER_CARD),
+                arrayOf("2537", CardBrand.MASTER_CARD),
+                arrayOf("2720", CardBrand.MASTER_CARD),
+                arrayOf("5151222233334444", CardBrand.MASTER_CARD),
+                arrayOf("2221222233334444", CardBrand.MASTER_CARD),
+                arrayOf("22212222333344445555", CardBrand.MASTER_CARD),
+                // JCB
+                arrayOf("35", CardBrand.JCB),
+                arrayOf("3511", CardBrand.JCB),
+                arrayOf("3511222233334444", CardBrand.JCB),
+                arrayOf("35112222333344445555", CardBrand.JCB),
+                // AMEX
+                arrayOf("34", CardBrand.AMEX),
+                arrayOf("37", CardBrand.AMEX),
+                arrayOf("340", CardBrand.AMEX),
+                arrayOf("3718", CardBrand.AMEX),
+                arrayOf("341122223333444", CardBrand.AMEX),
+                arrayOf("34112222333344445555", CardBrand.AMEX),
+                // DINERS_CLUB
+                arrayOf("300", CardBrand.DINERS_CLUB),
+                arrayOf("301", CardBrand.DINERS_CLUB),
+                arrayOf("302", CardBrand.DINERS_CLUB),
+                arrayOf("303", CardBrand.DINERS_CLUB),
+                arrayOf("304", CardBrand.DINERS_CLUB),
+                arrayOf("305", CardBrand.DINERS_CLUB),
+                arrayOf("36", CardBrand.DINERS_CLUB),
+                arrayOf("38", CardBrand.DINERS_CLUB),
+                arrayOf("3001", CardBrand.DINERS_CLUB),
+                arrayOf("3052", CardBrand.DINERS_CLUB),
+                arrayOf("363", CardBrand.DINERS_CLUB),
+                arrayOf("384", CardBrand.DINERS_CLUB),
+                arrayOf("30012222333344", CardBrand.DINERS_CLUB),
+                arrayOf("38012222333344", CardBrand.DINERS_CLUB),
+                arrayOf("38012222333344445555", CardBrand.DINERS_CLUB),
+                // DISCOVER
+                arrayOf("6011", CardBrand.DISCOVER),
+                arrayOf("65", CardBrand.DISCOVER),
+                arrayOf("60112", CardBrand.DISCOVER),
+                arrayOf("650", CardBrand.DISCOVER),
+                arrayOf("6011222233334444", CardBrand.DISCOVER),
+                arrayOf("6511222233334444", CardBrand.DISCOVER),
+                arrayOf("65112222333344445555", CardBrand.DISCOVER)
             )
         }
     }
