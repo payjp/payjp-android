ad499fcd1ba2d893bad6db93d6fdd05d6dfd1b6d
yatatsu <yatatsukitagawa@gmail.com>
2019-07-24 11:28:39 +0900

    Test card number format with brands.

--- payjp/src/test/kotlin/jp/pay/android/ui/widget/CardNumberFormatTextWatcherTest.kt
+++ payjp/src/test/kotlin/jp/pay/android/ui/widget/CardNumberFormatTextWatcherTest.kt
@@ -23,17 +23,19 @@
 package jp.pay.android.ui.widget
 
 import android.widget.EditText
+import androidx.test.core.app.ApplicationProvider
+import jp.pay.android.model.CardBrand
 import org.hamcrest.Matchers.`is`
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.ParameterizedRobolectricTestRunner
-import org.robolectric.RuntimeEnvironment
 
 @RunWith(ParameterizedRobolectricTestRunner::class)
 class CardNumberFormatTextWatcherTest(
     private val input: String?,
+    private val brand: CardBrand?,
     private val result: String?
 ) {
 
@@ -43,12 +45,20 @@ class CardNumberFormatTextWatcherTest(
         @ParameterizedRobolectricTestRunner.Parameters
         fun data(): List<Array<out Any?>> {
             return listOf(
-                arrayOf("4242424242424242", "4242 4242 4242 4242"),
-                arrayOf("42", "42"),
-                arrayOf("42424", "4242 4"),
-                arrayOf("424242aaa", "4242 42"),
-                arrayOf("aaaa", ""),
-                arrayOf("4242424242424242111", "4242 4242 4242 4242")
+                arrayOf("1234123412341234", null, "1234 1234 1234 1234"),
+                arrayOf("12", null, "12"),
+                arrayOf("12345", null, "1234 5"),
+                arrayOf("123456aaa", null, "1234 56"),
+                arrayOf("aaaa", null, ""),
+                arrayOf("123412341234123412", null, "1234 1234 1234 1234"),
+                arrayOf("1234123412341234", CardBrand.VISA, "1234 1234 1234 1234"),
+                arrayOf("1234123412341234", CardBrand.MASTER_CARD, "1234 1234 1234 1234"),
+                arrayOf("1234123412341234", CardBrand.JCB, "1234 1234 1234 1234"),
+                arrayOf("1234123412341234", CardBrand.DISCOVER, "1234 1234 1234 1234"),
+                arrayOf("123412345612345", CardBrand.AMEX, "1234 123456 12345"),
+                arrayOf("12341234561234", CardBrand.DINERS_CLUB, "1234 123456 1234"),
+                arrayOf("123456", CardBrand.AMEX, "1234 56"),
+                arrayOf("123456", CardBrand.DINERS_CLUB, "1234 56")
             )
         }
     }
@@ -59,13 +69,16 @@ class CardNumberFormatTextWatcherTest(
     @Before
     fun setUp() {
         textWatcher = CardNumberFormatTextWatcher(' ')
-        editText = EditText(RuntimeEnvironment.application)
+        editText = EditText(ApplicationProvider.getApplicationContext())
         editText.addTextChangedListener(textWatcher)
     }
 
     @Test
     fun formatInput() {
+        if (brand != null) {
+            textWatcher.brand = brand
+        }
         editText.setText(input)
-        assertThat(editText.text.toString(), `is`(result))
+        assertThat("input = $input, brand = $brand", editText.text.toString(), `is`(result))
     }
 }
