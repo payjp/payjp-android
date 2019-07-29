06e21b740da3350e1396fa5c334c6abe8057f18c
yatatsu <yatatsukitagawa@gmail.com>
2019-07-29 13:01:03 +0900

    Add ViewModel.

---
+++ payjp/src/main/kotlin/jp/pay/android/ui/widget/CardFormViewModel.kt
@@ -0,0 +1,149 @@
+/*
+ *
+ * Copyright (c) 2019 PAY, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package jp.pay.android.ui.widget
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ViewModel
+import jp.pay.android.PayjpTokenService
+import jp.pay.android.Task
+import jp.pay.android.exception.PayjpInvalidCardFormException
+import jp.pay.android.model.AcceptedBrandsResponse
+import jp.pay.android.model.CardBrand
+import jp.pay.android.model.CardComponentInput
+import jp.pay.android.model.CardCvcInput
+import jp.pay.android.model.CardExpirationInput
+import jp.pay.android.model.CardHolderNameInput
+import jp.pay.android.model.CardNumberInput
+import jp.pay.android.model.TenantId
+import jp.pay.android.model.Token
+import jp.pay.android.ui.widget.CardComponentInputView.OnChangeInputListener
+import jp.pay.android.util.Tasks
+
+internal interface CardFormViewModelOutput {
+    val cardNumberInput: LiveData<CardNumberInput>
+    val cardExpirationInput: LiveData<CardExpirationInput>
+    val cardCvcInput: LiveData<CardCvcInput>
+    val cardHolderNameInput: LiveData<CardHolderNameInput>
+    val cardHolderNameEnabled: LiveData<Boolean>
+    val acceptedBrands: LiveData<List<CardBrand>>
+    val isValid: LiveData<Boolean>
+}
+
+internal interface CardFormViewModelInput {
+
+    fun setTenantId(tenantId: TenantId?)
+
+    fun updateCardHolderNameEnabled(enabled: Boolean)
+
+    fun createToken(): Task<Token>
+}
+
+internal class CardFormViewModel(
+    private val tokenService: PayjpTokenService
+) : ViewModel(), CardFormViewModelOutput, CardFormViewModelInput, LifecycleObserver,
+    OnChangeInputListener<CardComponentInput<out Any>> {
+
+    override val cardNumberInput = MutableLiveData<CardNumberInput>()
+    override val cardExpirationInput = MutableLiveData<CardExpirationInput>()
+    override val cardCvcInput = MutableLiveData<CardCvcInput>()
+    override val cardHolderNameInput = MutableLiveData<CardHolderNameInput>()
+    override val cardHolderNameEnabled = MutableLiveData<Boolean>()
+    override val acceptedBrands = MutableLiveData<List<CardBrand>>()
+    override val isValid: LiveData<Boolean>
+
+    private var task: Task<AcceptedBrandsResponse>? = null
+    private var tenantId: TenantId? = null
+
+    init {
+        isValid = MediatorLiveData<Boolean>().apply {
+            fun checkValid() = cardNumberInput.value?.valid == true &&
+                cardExpirationInput.value?.valid == true &&
+                cardCvcInput.value?.valid == true &&
+                (cardHolderNameEnabled.value == false || cardHolderNameInput.value?.valid == true)
+            addSource(cardNumberInput) { value = checkValid() }
+            addSource(cardExpirationInput) { value = checkValid() }
+            addSource(cardCvcInput) { value = checkValid() }
+            addSource(cardHolderNameInput) { value = checkValid() }
+            addSource(cardHolderNameEnabled) { value = checkValid() }
+        }
+    }
+
+    override fun onCleared() {
+        task?.cancel()
+        task = null
+    }
+
+    override fun onChangeInput(input: CardComponentInput<out Any>) {
+        val data = when (input) {
+            is CardNumberInput -> cardNumberInput
+            is CardExpirationInput -> cardExpirationInput
+            is CardCvcInput -> cardCvcInput
+            is CardHolderNameInput -> cardHolderNameInput
+            else -> throw IllegalStateException("unknown input")
+        }
+        data.value = input
+    }
+
+    override fun setTenantId(tenantId: TenantId?) {
+        this.tenantId = tenantId
+    }
+
+    override fun updateCardHolderNameEnabled(enabled: Boolean) {
+        this.cardHolderNameEnabled.value = enabled
+    }
+
+    override fun createToken(): Task<Token> {
+        return if (isValid.value == true) {
+            tokenService.createToken(
+                number = checkNotNull(cardNumberInput.value?.value),
+                expMonth = checkNotNull(cardExpirationInput.value?.value).month,
+                expYear = checkNotNull(cardExpirationInput.value?.value).year,
+                cvc = checkNotNull(cardCvcInput.value?.value),
+                name = checkNotNull(cardHolderNameInput.value?.value)
+            )
+        } else {
+            Tasks.failure(
+                PayjpInvalidCardFormException("Card form is not valid")
+            )
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun fetchAcceptedBrands() {
+        if (acceptedBrands.value != null) {
+            task = tokenService.getAcceptedBrands(tenantId)
+            task?.enqueue(object : Task.Callback<AcceptedBrandsResponse> {
+                override fun onSuccess(data: AcceptedBrandsResponse) {
+                    acceptedBrands.postValue(data.brands)
+                }
+
+                override fun onError(throwable: Throwable) {}
+            })
+        }
+    }
+}
