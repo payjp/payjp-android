a2af733b29c9e96b004ca0b94da050ca562b7e10
yatatsu <yatatsukitagawa@gmail.com>
2020-09-29 17:26:12 +0900

    Generate docs

---
+++ docs/payjp-android-core/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":payjp-android-core:dokkaHtml/debug": [], ":payjp-android-core:dokkaHtml/main": [], ":payjp-android-core:dokkaHtml/release": []}'
---
+++ docs/payjp-android-main/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":payjp-android-main:dokkaHtml/debug": [], ":payjp-android-main:dokkaHtml/main": [], ":payjp-android-main:dokkaHtml/release": []}'
